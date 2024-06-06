bcad732d4e41f7d99125b547e56e9089ae022ac3
Katie Bug <katierosealford@gmail.com>
2024-06-05 21:34:14 -0400

    updated min length on thought to match criteria

--- models/thought.js
+++ models/thought.js
@@ -6,7 +6,7 @@ const thoughtSchema = new Schema(
     thoughtText: {
       type: String,
       required: true,
-      minlength: 2,
+      minlength: 1,
       maxlength: 280,
     },
     createdAt: {
