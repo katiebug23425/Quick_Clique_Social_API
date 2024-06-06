7b1520e2d000c942494885b2ac8b900a917d66a0
Katie Bug <katierosealford@gmail.com>
2024-06-05 20:29:48 -0400

    wrote routes but still working on them

---
+++ routes/api-routes/user-routes.js
@@ -0,0 +1,22 @@
+//imports
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUserById,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/user-controller');
+
+// Define the routes for GET and POST all Users
+router.route('/').get(getAllUsers).post(createUser);
+
+// GET user id, PUT update user id and DELETE user by id
+router.route('/:userId').get(getUserById).put(updateUserById).delete(deleteUserById);
+
+// POST add friend and DELETE remove Friend
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+// Export the router
+module.exports = router;
