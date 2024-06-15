4f7af7aa3a92449359ac546956ac73a62416469c
Katie Bug <katierosealford@gmail.com>
2024-06-15 08:27:46 -0400

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api-routes');
 router.use('/api', apiRoutes);
-router.use((req, res) => {
- return res.send("<h1>Wrong Route!</h1>");
-});
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
